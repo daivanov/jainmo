3a036aa869b15a03f0a54a0f1e428876121b3996
Daniil Ivanov <daniil.ivanov@gmail.com>
2015-08-31 21:11:36 +0300

    Problem with resolving generic parameters of constructor parameters

--- src/test/java/uk/co/jemos/podam/test/dto/GenericInConstructorPojo.java
+++ src/test/java/uk/co/jemos/podam/test/dto/GenericInConstructorPojo.java
@@ -9,13 +9,25 @@ import java.util.Vector;
  */
 public class GenericInConstructorPojo {
 
-	private Vector<String> vector;
+	private Vector<?> objVector;
+	private Vector<String> strVector;
+	private Vector<Integer> intVector;
 
-	public GenericInConstructorPojo(Vector<String> vector) {
-		this.vector = vector;
+	public GenericInConstructorPojo(Vector<?> objVector, Vector<String> strVector, Vector<Integer> intVector) {
+		this.objVector = objVector;
+		this.strVector = strVector;
+		this.intVector = intVector;
 	}
 
-	public Vector<String> getVector() {
-		return vector;
+	public Vector<?> getObjVector() {
+		return objVector;
+	}
+
+	public Vector<String> getStrVector() {
+		return strVector;
+	}
+
+	public Vector<Integer> getIntVector() {
+		return intVector;
 	}
 }
--- src/test/java/uk/co/jemos/podam/test/unit/ConstructorsUnitTest.java
+++ src/test/java/uk/co/jemos/podam/test/unit/ConstructorsUnitTest.java
@@ -1,6 +1,7 @@
 package uk.co.jemos.podam.test.unit;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import uk.co.jemos.podam.api.PodamFactory;
@@ -20,15 +21,30 @@ public class ConstructorsUnitTest {
 
 	private static final PodamFactory factory = new PodamFactoryImpl();
 
+	@Before
+	public void cleanup() {
+		factory.getStrategy().clearMemoizationCache();
+	}
+
 	@Test
 	public void testGenericInConstructorPojoInstantiation() {
 		GenericInConstructorPojo pojo
 				= factory.manufacturePojo(GenericInConstructorPojo.class);
 		Assert.assertNotNull("Instantiation failed", pojo);
-		Assert.assertNotNull("Field instantiation failed", pojo.getVector());
-		for (Object element : pojo.getVector()) {
+		Assert.assertNotNull("object vector instantiation failed", pojo.getObjVector());
+		Assert.assertNotNull("string vector instantiation failed", pojo.getStrVector());
+		Assert.assertNotNull("integer vector instantiation failed", pojo.getIntVector());
+		for (Object element : pojo.getObjVector()) {
+			Assert.assertEquals("Wrong collection element type",
+					Object.class, element.getClass());
+		}
+		for (Object element : pojo.getStrVector()) {
 			Assert.assertEquals("Wrong collection element type",
 					String.class, element.getClass());
+		}
+		for (Object element : pojo.getIntVector()) {
+			Assert.assertEquals("Wrong collection element type",
+					Integer.class, element.getClass());
 		}
 	}
 
